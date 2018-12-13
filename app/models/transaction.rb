8bf4fc118dfcdd26807cd2525f6dffe5ceb8bf39
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2018-12-12 18:50:52 -0600

    Created transaction model

---
+++ app/models/transaction.rb
@@ -0,0 +1,15 @@
+class Transaction < ApplicationRecord
+
+  belongs_to :user
+
+  enum type_of_crypto: [ :bitcoin, :ethereum ]
+  enum purchase_type:  [ :cad, :number_of_coin]
+
+  def user_email
+    if self.guest_email.present?
+      self.guest_email
+    else
+      self.user.email
+    end
+  end
+end
