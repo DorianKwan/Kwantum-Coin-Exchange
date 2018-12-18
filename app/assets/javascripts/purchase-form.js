d5fe1c9d9fb947ff3ad7dc70d61230fa1dcfc6e1
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2018-12-17 18:03:14 -0600

    Edited purchase form javascript

--- app/assets/javascripts/purchase-form.js
+++ app/assets/javascripts/purchase-form.js
@@ -2,24 +2,38 @@ document.addEventListener("turbolinks:load", function () {
   
   // This function will toggle the hidden class on two elements
   function toggleHidden(unhideElement, hideElement) {
-    if (unhideElement.hasClass('hidden')) { unhideElement.removeClass('hidden'); }
-    if (!hideElement.hasClass('hidden')) { hideElement.addClass('hidden'); }
+    if ($(unhideElement).hasClass('hidden')) { $(unhideElement).removeClass('hidden'); }
+    if (!$(hideElement).hasClass('hidden')) { $(hideElement).addClass('hidden'); }
+  }
+
+  // This function will update the coin/cash totals in form
+  function updateTotals(crypto, amountOfCrypto, amountOfCash) {
+    var cryptoSymbol = (crypto === 'Bitcoin' ? '₿' : 'Ξ');
+    $('.cash-amount').val(amountOfCash);
+    $('.coin-amount').val(amountOfCrypto);
+    $('.order-total-hidden').val(amountOfCash);
+    $('.crypto-amount-visible').text(`${cryptoSymbol} ${amountOfCrypto}`);
+    $('.order-total').text(amountOfCash.toFixed(2));
+  }
+
+  // This function will reset the coin/cash totals in form
+  function resetTotals(crypto) {
+    var cryptoSymbol = (crypto === 'Bitcoin' ? '₿' : 'Ξ');
+    $('.cash-amount').val('');
+    $('.coin-amount').val('');
+    $('.order-total-hidden').val(0);
+    $('.crypto-amount-visible').text(`${cryptoSymbol} 0.00`);
+    $('.order-total').text(0.00);
   }
 
   if ($('#new-transaction').length) {
-
-    var cashAmount   = $('.cash-amount');
-    var coinAmount   = $('.coin-amount');
-    var hiddenTotal  = $('.order-total-hidden');
     var btcValue     = $('.btc-value');
     var ethValue     = $('.eth-value');
-    var hideForm     = $('.hide-form');
+    var orderForm    = $('#order-form');
     var ptToggleDiv  = $('.purchase-type-toggle');
     var purchaseType = $('.purchase-type');
     var typeOfCrypto = $('.type-of-crypto');
-    var visibleCoin  = document.getElementsByClassName('crypto-amount-visible')[0];
     var amountlabel  = document.getElementsByClassName('amount-label')[0];
-    var cashTotal    = document.getElementsByClassName('order-total')[0];
     var selectValue  = document.getElementsByClassName('select-value')[0];
     var btcSelect    = document.getElementsByClassName('bitcoin-select')[0];
     var ethSelect    = document.getElementsByClassName('ethereum-select')[0];
@@ -29,58 +43,55 @@ document.addEventListener("turbolinks:load", function () {
     btcSelect.addEventListener('click', function () {
       selectValue.innerHTML = "Bitcoin";
       toggleHidden(btcValue, ethValue);
-      if (hideForm.hasClass('hidden')) { hideForm.removeClass('hidden'); }
+      if (orderForm.hasClass('hidden')) { orderForm.removeClass('hidden'); }
       typeOfCrypto.val('bitcoin');
+      $('.cash-amount').keyup();
     });
     
     ethSelect.addEventListener('click', function () {
       selectValue.innerHTML = "Ethereum";
       toggleHidden(ethValue, btcValue);
-      if (hideForm.hasClass('hidden')) { hideForm.removeClass('hidden'); }
+      if (orderForm.hasClass('hidden')) { orderForm.removeClass('hidden'); }
       typeOfCrypto.val('ethereum');
+      $('.cash-amount').keyup();
     });
-
+    
     ptToggleDiv.unbind('click').bind('click', function () {
       var cryptocurrency = selectValue.innerHTML;
       if (!$('#input-toggle').prop('checked')) {
         amountlabel.innerHTML = `Enter the desired amount of ${cryptocurrency} in CAD:`;
-        toggleHidden(cashAmount, coinAmount);
+        toggleHidden('.cash-amount', '.coin-amount');
         purchaseType.val('cad');
       } else {
         amountlabel.innerHTML = `Enter the desired amount of ${cryptocurrency}`;
-        toggleHidden(coinAmount, cashAmount);
+        toggleHidden('.coin-amount', '.cash-amount');
         purchaseType.val('crypto');
       }
     });
 
     // Handle amount of CAD input, updates hidden input values for form
-    cashAmount.unbind('keyup').bind('keyup', function () {
-      var cash = parseFloat(cashAmount.val());
+    $('.cash-amount').unbind('keyup').bind('keyup', function () {
       var cryptocurrency = selectValue.innerHTML;
+      var amountOfCash   = parseFloat($(this).val());
       var cryptoPrice    = (cryptocurrency === "Bitcoin" ? btcPrice : ethPrice);
-      var cryptoSymbol   = (cryptocurrency === "Bitcoin" ? '₿' : 'Ξ');
-      var amountOfCrypto = Math.round((cash / parseFloat(cryptoPrice)) * 100000) / 100000;
+      var amountOfCrypto = Math.round((amountOfCash / parseFloat(cryptoPrice)) * 100000) / 100000;
       if (amountOfCrypto) {
-        coinAmount.val(amountOfCrypto);
-        hiddenTotal.val(cashAmount.val());
-        visibleCoin.innerHTML = `${cryptoSymbol} ${amountOfCrypto}`;
-        cashTotal.innerHTML   = parseFloat(cashAmount.val()).toFixed(2);
+        updateTotals(cryptocurrency, amountOfCrypto, amountOfCash);
+      } else {
+        resetTotals(cryptocurrency);
       }
     });
     
     // Handle amount of coins input, updates hidden input values for form
-    coinAmount.unbind('keyup').bind('keyup', function () {
+    $('.coin-amount').unbind('keyup').bind('keyup', function () {
       var cryptocurrency = selectValue.innerHTML;
-      var amountOfCrypto = parseFloat(coinAmount.val());
-      var cryptoPrice  = (cryptocurrency === "Bitcoin" ? btcPrice : ethPrice);
-      var cryptoSymbol = (cryptocurrency === "Bitcoin" ? '₿' : 'Ξ');
-      var amountOfCash = Math.round((amountOfCrypto * parseFloat(cryptoPrice)) * 100) / 100;
+      var amountOfCrypto = parseFloat($(this).val());
+      var cryptoPrice    = (cryptocurrency === "Bitcoin" ? btcPrice : ethPrice);
+      var amountOfCash   = Math.round((amountOfCrypto * parseFloat(cryptoPrice)) * 100) / 100;
       if (amountOfCash) {
-        cashAmount.val(amountOfCash);
-        coinAmount.val(amountOfCrypto);
-        hiddenTotal.val(amountOfCash);
-        visibleCoin.innerHTML = `${cryptoSymbol} ${amountOfCrypto}`;
-        cashTotal.innerHTML   = amountOfCash.toFixed(2);
+        updateTotals(cryptocurrency, amountOfCrypto, amountOfCash);
+      } else {
+        resetTotals(cryptocurrency);
       }
     });
   }
