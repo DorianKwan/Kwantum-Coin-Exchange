067b34b5d7b7932165ad60ae85b57aaa4a141ace
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2019-12-08 23:19:38 -0800

    Fixed js script bug for homepage img swapping

--- app/assets/javascripts/background-img-swap.js
+++ app/assets/javascripts/background-img-swap.js
@@ -2,7 +2,7 @@ document.addEventListener("turbolinks:load", function () {
   // Dynamically change the z-index of a fixed background img
   document.addEventListener('scroll', function() {
     // Only run when on homepage
-    if (('#homepage-section').length) {
+    if ($('#homepage-section').length) {
       // Scroll height from window top to top of page
       var currentScrollHeight = jQuery(window).scrollTop();
       var heightWhenImgSwitch = jQuery('#join-us').offset().top - jQuery(window).height() - 50;
