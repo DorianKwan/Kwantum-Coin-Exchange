6aaecb5933e49afd91f86b5170b7f0ade2c2fee8
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2019-12-10 15:09:25 -0800

    Added middleware config for rails_admin admin panel

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,6 +8,10 @@ RailsAdmin.config do |config|
   end
   config.current_user_method(&:current_user)
 
+  config.authorize_with do
+    redirect_to main_app.root_path unless warden.user.admin?
+  end
+
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   config.actions do
