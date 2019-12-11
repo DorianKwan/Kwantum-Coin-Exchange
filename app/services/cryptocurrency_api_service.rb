207ca8d7fbdd668c42d93077ca97f2633cb0f06c
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2019-12-10 17:31:52 -0800

    Refactored api service by breaking down methods

--- app/services/cryptocurrency_api_service.rb
+++ app/services/cryptocurrency_api_service.rb
@@ -2,21 +2,31 @@ class CryptocurrencyApiService
   def initialize
   end
 
-  def get_cryptocurreny_data(dollar_type='CAD,USD')
-    cryptos = Cryptocurrency.all.pluck(:symbol).map { |symbol| symbol.upcase }.join(',') 
+  def update_bulk_cryptocurrency_data
+    symbols = Cryptocurrency.all.pluck(:symbol).map { |symbol| symbol.upcase }.join(',')
+    json = send_api_request(symbols)
+    symbols.split(',').each { |symbol| update_cryptocurrency(symbol, json) } if json
+  end
+
+  def update_single_cryptocurrency_data(symbol)
+    json = send_api_request(symbol)
+    update_cryptocurrency(symbol, json)
+  end
+
+  private
+
+  def send_api_request(symbols, dollar_type='CAD,USD')
     begin
-      url  = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{cryptos}&tsyms=#{dollar_type}";
+      url  = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{symbols}&tsyms=#{dollar_type}";
       url += "&api_key=#{ENV['CRYPTO_API_KEY']}"
       uri  = URI(url)
       res  = Net::HTTP.get(uri)
       json = JSON.parse(res)
     rescue => e  
+      puts "Raised exception requesting cryptocurrency data url: #{url}"
       puts e.message  
       puts e.backtrace.inspect
-      return
     end
-
-    cryptos.split(',').each { |crypto| self.update_cryptocurrency(crypto, json) }
   end
 
   def update_cryptocurrency(symbol, api_data)
